e1a19834ae1ceda1f87bf2dda61c01b48d2d4f93
KhalidNoman <ksnoman@live.com>
2021-04-22 19:00:45 +0300

    Added drag and drop functionality to make it more user friendly.

--- app/src/main/java/com/example/codingapplication/examples_Conditionals.java
+++ app/src/main/java/com/example/codingapplication/examples_Conditionals.java
@@ -1,5 +1,6 @@
 package com.example.codingapplication;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 import androidx.core.content.ContextCompat;
@@ -7,13 +8,18 @@ import androidx.core.content.ContextCompat;
 import android.annotation.SuppressLint;
 import android.content.ClipData;
 import android.content.ClipDescription;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
 import android.view.DragEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.Switch;
@@ -30,9 +36,12 @@ public class examples_Conditionals extends AppCompatActivity {
     private TextView exaDragCase;
     private TextView exaDragBreak;
 
-
     private ScrollView exaScroller;
     private LinearLayout exaCondContainer;
+
+    private Button exaCondButton;
+    private TextView exaCondResult;
+
     boolean hasIf, hasSwitch = false;
     String dragged = "";
 
@@ -52,6 +61,12 @@ public class examples_Conditionals extends AppCompatActivity {
 
         exaScroller = (ScrollView) findViewById(R.id.exaScroller);
         exaCondContainer = (LinearLayout) findViewById(R.id.exaCondContainer);
+
+        exaCondButton = (Button) findViewById(R.id.exaCondButton);
+        exaCondResult = (TextView) findViewById(R.id.exaCondResult);
+
+
+        final String[] userVal = {"0","0","0","0"};
 
         exaDragIf.setOnTouchListener((view, motionEvent) -> {
             if(motionEvent.getAction() == MotionEvent.ACTION_DOWN){
@@ -152,18 +167,104 @@ public class examples_Conditionals extends AppCompatActivity {
                         //Ifs 
                         if(dragData.toString().equals("if")) {
                             hasIf = true;
+
+                            AlertDialog.Builder userAlert = new AlertDialog.Builder(examples_Conditionals.this);
+                            userAlert.setTitle("Create your conditional!");
+
                             TextView tv=new TextView(examples_Conditionals.this);
-                            tv.setText(dragData.toString().concat(" () {"));
-                            tv.setTextSize(30);
+                            //tv.setText("int x = " + userVal[0] + "\n" + dragData.toString().concat(" (x >" + userVal[1] + ") {"));
+                            tv.setTextSize(20);
                             exaCondContainer.addView(tv);
 
-                            ((ViewGroup) exaDragSwitch.getParent()).removeView(exaDragSwitch);
-                            ((ViewGroup) exaDragCase.getParent()).removeView(exaDragCase);
-                            ((ViewGroup) exaDragBreak.getParent()).removeView(exaDragBreak);
+                            Context con = examples_Conditionals.this;
+                            LinearLayout conLayout = new LinearLayout(con);
+                            conLayout.setOrientation(LinearLayout.VERTICAL);
+
+                            EditText valueHolder = new EditText(con);
+                            valueHolder.setHint("Variable value");
+                            conLayout.addView(valueHolder);
+
+                            EditText ifPart = new EditText(con);
+                            ifPart.setHint("if condition");
+                            conLayout.addView(ifPart);
+
+                            /*EditText elseIfPart = new EditText(con);
+                            elseIfPart.setHint("else if condition");
+                            conLayout.addView(elseIfPart);
+                            */
+
+                            userAlert.setView(conLayout);
+                            userAlert.setPositiveButton("Submit", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    userVal[0] = valueHolder.getText().toString();
+                                    userVal[1] = ifPart.getText().toString();
+
+                                    tv.setText("int x = " + userVal[0] + ";\n" + dragData.toString().concat(" (x <" + userVal[1] + ") {\n" + "\t\tSystem.out.println(\"if satisfied\");\n}"));
+                                    ((ViewGroup) exaDragIf.getParent()).removeView(exaDragIf);
+                                    ((ViewGroup) exaDragSwitch.getParent()).removeView(exaDragSwitch);
+                                    ((ViewGroup) exaDragCase.getParent()).removeView(exaDragCase);
+                                    ((ViewGroup) exaDragBreak.getParent()).removeView(exaDragBreak);
+                                }
+                            });
+                            userAlert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                }
+                            });
+                            userAlert.show();
+
                         } else if(dragData.toString().equals("else if")){
 
-                        } else if(dragData.toString().equals("else")){
+                            AlertDialog.Builder userAlert = new AlertDialog.Builder(examples_Conditionals.this);
+                            userAlert.setTitle("Create your conditional!");
 
+                            TextView tv=new TextView(examples_Conditionals.this);
+                            tv.setTextSize(20);
+                            exaCondContainer.addView(tv);
+
+                            Context con = examples_Conditionals.this;
+                            LinearLayout conLayout = new LinearLayout(con);
+                            conLayout.setOrientation(LinearLayout.VERTICAL);
+
+                            EditText elseIfPart = new EditText(con);
+                            elseIfPart.setHint("else if condition");
+                            elseIfPart.setInputType(InputType.TYPE_CLASS_NUMBER);
+                            conLayout.addView(elseIfPart);
+
+
+                            userAlert.setView(conLayout);
+                            userAlert.setPositiveButton("Submit", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    userVal[2] = elseIfPart.getText().toString();
+
+                                    tv.setText(dragData.toString().concat(" (x >" + userVal[2] + ") {\n" + "\t\tSystem.out.println(\"else if satisfied\");\n}"));
+                                    ((ViewGroup) exaDragElseIf.getParent()).removeView(exaDragElseIf);
+                                }
+                            });
+                            userAlert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                }
+                            });
+                            userAlert.show();
+
+
+
+
+                        } else if(dragData.toString().equals("else")){
+                            AlertDialog.Builder userAlert = new AlertDialog.Builder(examples_Conditionals.this);
+                            userAlert.setTitle("Create your conditional!");
+
+                            TextView tv=new TextView(examples_Conditionals.this);
+                            tv.setText(dragData.toString().concat("{\n" + "\t\tSystem.out.println(\"else satisfied\");\n}"));
+                            tv.setTextSize(20);
+                            exaCondContainer.addView(tv);
+
+                            ((ViewGroup) exaDragElse.getParent()).removeView(exaDragElse);
                         }
                         //Switches
                         else if (dragData.toString().equals("switch")) {
@@ -183,6 +284,16 @@ public class examples_Conditionals extends AppCompatActivity {
                     default:
                         return false;
                 }
+            }
+        });
+
+        exaCondButton.setOnClickListener((view) -> {
+            if(Integer.parseInt(userVal[0]) > Integer.parseInt(userVal[1])){
+                exaCondResult.setText("if satisfied");
+            } else if (Integer.parseInt(userVal[0]) < Integer.parseInt(userVal[2])){
+                exaCondResult.setText("else if satisfied");
+            } else {
+                exaCondResult.setText("else satisfied");
             }
         });
 
